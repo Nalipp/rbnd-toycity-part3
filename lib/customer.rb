7d57b4b098e6de2181421ad2b8093a54ecb02925
Nalipp <nateplipp@gmail.com>
2016-04-03 09:45:39 +0900

    create customer methods

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,1 +1,23 @@
+class Customer
+  attr_accessor :name
 
+  @@customers = []
+
+  def initialize(options={})
+    @name = options[:name]
+    add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+
+  def add_to_customers
+    # @@customers.each { |customer| raise DuplicateCustomerError, " '#{name}' is already in the database" if customer.name == name }
+    @@customers << self
+  end
+
+  def self.find_by_name(customer_name)
+    @@customers.map { |customer| return customer if customer.name = customer_name}
+  end
+end
